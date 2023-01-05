0b3ea546a59d4ceb1264c43061df044119e3ade2
SoinRoma <soinroma26@gmail.com>
2023-01-05 10:37:21 +0500

    - added full CRUD

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
 const Post = require('./models/post');
 const Contact = require('./models/contact');
 
@@ -33,6 +34,7 @@ app.use(express.urlencoded({
 }));
 
 app.use(express.static('assets'));
+app.use(methodOverride('_method'));
 
 app.get('/', (req, res) => {
   const title = 'Home';
@@ -77,6 +79,60 @@ app.get('/posts/:id', (req, res) => {
     })
 });
 
+app.delete('/posts/:id', (req, res) => {
+  Post
+    .findByIdAndDelete(req.params.id)
+    .then((result) => {
+      res.sendStatus(200);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.render(createPath('error'), {
+        title: 'Error'
+      });
+    });
+});
+
+
+app.get('/edit/:id', (req, res) => {
+  const title = 'Edit Post';
+  Post
+    .findById(req.params.id)
+    .then(post => res.render(createPath('edit-post'), {
+      post,
+      title
+    }))
+    .catch((error) => {
+      console.log(error);
+      res.render(createPath('error'), {
+        title: 'Error'
+      });
+    });
+});
+
+app.put('/edit/:id', (req, res) => {
+  const {
+    title,
+    author,
+    text
+  } = req.body;
+  const {
+    id
+  } = req.params;
+  Post
+    .findByIdAndUpdate(id, {
+      title,
+      author,
+      text
+    })
+    .then((result) => res.redirect(`/posts/${id}`))
+    .catch((error) => {
+      console.log(error);
+      res.render(createPath('error'), {
+        title: 'Error'
+      });
+    });
+});
 
 
 app.get('/posts', (req, res) => {
